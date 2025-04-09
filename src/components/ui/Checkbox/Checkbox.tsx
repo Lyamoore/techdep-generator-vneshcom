f73dca1d3514b12cfe09848ff5c6b0cec11dc147
Mary <lyamooore@mail.ru>
2025-04-10 01:05:53 +0300

    Компонент <Checkbox />

--- src/components/ui/Checkbox/Checkbox.tsx
+++ src/components/ui/Checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import './Checkbox.css';
+import styles from './Checkbox.module.css';
 
 type Props = Omit<React.ComponentProps<'input'>, 'type' | 'children'> & {
   label: React.ReactNode;
@@ -6,16 +6,16 @@ type Props = Omit<React.ComponentProps<'input'>, 'type' | 'children'> & {
 
 export const Checkbox = ({ label, ...props }: Props) => {
   return (
-    <>
-      <input type="checkbox" className="checkbox" {...props} />
-      <label htmlFor={props.id} className="label">
-        <div className="castom">
+    <div className={styles.container}>
+      <input type="checkbox" className={styles.checkbox} {...props} />
+      <label htmlFor={props.id} className={styles.label}>
+        <div className={styles.castom}>
           <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none">
             <path d="M1 4.88L3.13333 6.8L7.4 2" stroke="#444444" strokeLinecap="round" />
           </svg>
         </div>
         {label}
       </label>
-    </>
+    </div>
   );
 };
