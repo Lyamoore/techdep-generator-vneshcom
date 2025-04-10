a2bd5b226acb62809d0760c6e871057c6035a0dc
Mary <lyamooore@mail.ru>
2025-04-10 04:26:28 +0300

    Компонент <Button />

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,6 +1,6 @@
-import './Button.css';
-
 import { ButtonHTMLAttributes } from 'react';
+
+import styles from './Button.module.css';
 
 interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'main' | 'secondary';
@@ -12,7 +12,7 @@ interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
 export const Button = ({ variant = 'secondary', size = 'full', children, disabled, type = 'button' }: IButton) => {
   return (
     <button
-      className={`button button--${variant} button--${size}`}
+      className={`${styles.button} ${styles[`button--${variant}`]} ${styles[`button--${size}`]}`}
       disabled={disabled}
       type={type === 'submit' ? 'submit' : 'button'}
     >
